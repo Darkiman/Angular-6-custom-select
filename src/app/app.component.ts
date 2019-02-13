b990a0a1d67a15e360522de7c8477fb8edefbd83
Alexey Gavrilov <agavrilov@wings.ai>
2019-02-13 18:11:18 +0300

    added using rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, Output} from '@angular/core';
 import { SelectMenuItem } from './types/selectMenuItem';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { DataService } from './services/data.service';
-import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
-import {LongNamePipe} from "./pipes/long-name.pipe";
+import { debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import {LongNamePipe} from './pipes/long-name.pipe';
+import { combineLatest } from 'rxjs/observable/combineLatest';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,7 @@ import {LongNamePipe} from "./pipes/long-name.pipe";
   styleUrls: ['./app.component.scss'],
   providers: [DataService, LongNamePipe]
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   @Output() formChanged: EventEmitter<any> = new EventEmitter();
 
   title = 'custom-select';
@@ -32,6 +34,12 @@ export class AppComponent {
   yearOptions = [];
   generationOptions = [];
 
+  modelSub: Subscription;
+  yearSub: Subscription;
+  generationSub: Subscription;
+  fullAutoNameSub: Subscription;
+  formChangedSub: Subscription;
+
   constructor(fb: FormBuilder, private dataService: DataService, private longNamePipe: LongNamePipe) {
     this.form = fb.group({
       fullAutoName: '',
@@ -50,7 +58,34 @@ export class AppComponent {
 
     this.brandOptions = this.dataService.getBrandOptions();
 
-    this.form.get('fullAutoName').valueChanges.pipe(
+    const brand$ = this.form.get('brand').valueChanges;
+    const model$ = this.form.get('model').valueChanges;
+    const year$ = this.form.get('year').valueChanges;
+
+    const modelCombined$ = combineLatest(brand$, (brand) => {
+      brand ? this.modelOptions = this.dataService.getModelOptionsByBrand(brand) : this.modelOptions = null;
+      return this.modelOptions;
+    });
+    const yearCombined$ = combineLatest(brand$, model$, (brand, model) => {
+      brand && model ? this.yearOptions = this.dataService.getYearOptionsByModel(model) : this.yearOptions = null;
+      return this.yearOptions;
+    });
+    const generationCombined$ = combineLatest(brand$, model$, year$, (brand, model, year) => {
+      brand && model && year ? this.generationOptions = this.dataService.getGenerationByYear(year) : this.generationOptions = null;
+      return this.generationOptions;
+    });
+
+    this.modelSub = modelCombined$.subscribe(val => {
+      console.log(val);
+    });
+    this.yearSub = yearCombined$.subscribe(val => {
+      console.log(val);
+    });
+    this.generationSub = generationCombined$.subscribe(val => {
+      console.log(val);
+    });
+
+    this.fullAutoNameSub = this.form.get('fullAutoName').valueChanges.pipe(
       debounceTime(400),
       distinctUntilChanged()
     ).subscribe(val => {
@@ -60,41 +95,8 @@ export class AppComponent {
         this.formChanged.emit(this.form.value);
       }
     });
-    this.form.get('brand').valueChanges.subscribe(val => {
-      if (val) {
-        this.modelOptions = this.dataService.getModelOptionsByBrand(val);
-        this.formChanged.emit(this.form.value);
-      }
-    });
-    this.form.get('model').valueChanges.subscribe(val => {
-      if (val) {
-        this.yearOptions = this.dataService.getYearOptionsByModel(val);
-        this.formChanged.emit(this.form.value);
-      } else {
-        this.yearOptions = null;
-      }
-    });
-    this.form.get('year').valueChanges.subscribe(val => {
-      if (val) {
-        this.generationOptions = this.dataService.getGenerationByYear(val);
-        this.formChanged.emit(this.form.value);
-      } else {
-        this.generationOptions = null;
-      }
-    });
-    this.form.get('generation').valueChanges.subscribe(val => {
-      if (val) {
-        this.formChanged.emit(this.form.value);
-      }
-    });
 
-    this.formChanged.subscribe((val) => {
-        console.log('Форма изменилась');
-        console.log(val);
-      }
-    )
-
-    this.form.valueChanges.pipe(
+    this.formChangedSub = this.form.valueChanges.pipe(
       debounceTime(500),
       distinctUntilChanged()
     ).subscribe(val => {
@@ -105,5 +107,23 @@ export class AppComponent {
   setFullName() {
     const result = this.longNamePipe.transform(this.form.value, ['brand', 'model', 'generation'], 'title');
     this.form.patchValue({fullAutoName: result}, {emitEvent: false});
+  }
+
+  ngOnDestroy() {
+    if (this.modelSub) {
+      this.modelSub.unsubscribe();
+    }
+    if (this.yearSub) {
+      this.yearSub.unsubscribe();
+    }
+    if (this.generationSub) {
+      this.generationSub.unsubscribe();
+    }
+    if (this.fullAutoNameSub) {
+      this.fullAutoNameSub.unsubscribe();
+    }
+    if (this.formChangedSub) {
+      this.formChangedSub.unsubscribe();
+    }
   }
 }
